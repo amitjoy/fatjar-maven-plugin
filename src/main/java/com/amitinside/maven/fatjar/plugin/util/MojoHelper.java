06d870a68b91c17cda4bc6454ffb627d46fa63ce
Amit Kumar Mondal <admin@amitinside.com>
2018-02-14 19:01:13 +0100

    Added preconditions to utility class

--- src/main/java/com/amitinside/maven/fatjar/plugin/util/MojoHelper.java
+++ src/main/java/com/amitinside/maven/fatjar/plugin/util/MojoHelper.java
@@ -9,6 +9,8 @@
  *******************************************************************************/
 package com.amitinside.maven.fatjar.plugin.util;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.Map;
 import java.util.Properties;
 
@@ -43,6 +45,9 @@ public final class MojoHelper {
     }
 
     public static String replaceVariable(final MavenProject project, String variable) {
+        checkNotNull(project, "Maven Project instance cannot be null");
+        checkNotNull(variable, "Variable cannot be null");
+
         variable = variable.substring(2, variable.length() - 1);
         if ("${user.home}".equalsIgnoreCase(variable)) {
             return getUserHome();
@@ -54,6 +59,9 @@ public final class MojoHelper {
     }
 
     public static String getVersion(final Properties properties, final String property) {
+        checkNotNull(properties, "Properties instance cannot be null");
+        checkNotNull(property, "Property cannot be null");
+
         if (property.indexOf('$') == -1) {
             return property;
         }
